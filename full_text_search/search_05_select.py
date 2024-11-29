0595797b4f783ebbceebbab1164e581050ffdaf5
canislatranscoxus <canislatranscoxus@gmail.com>
2024-11-28 23:26:25 -0600

    Full Text Search - Select

--- full_text_search/search_05_select.py
+++ full_text_search/search_05_select.py
@@ -1,34 +1,78 @@
 import json
+import os
 import sys
+
 import redis
 import redis.commands.search
 import redis.commands.search
 import redis.commands.search.aggregation as aggs
-import redis.commands.search.reducers as reducers
-from   redis.commands.search.query  import NumericFilter, Query
+import redis.commands.search.reducers    as reducers
+from   redis.commands.search.query       import NumericFilter, Query
+from   redis.commands.search.querystring import querystring
 
-class Cool_class:
 
-    def select_order(self, client_id ):
-        # Get the last 10 orders of the client. Only order, no items. See sql
-        #
-        # SELECT *
-        # FROM my_order_index
-        # WHERE client_id =   <client_id>
-        # and row_num between offset and (offset + num)
-        # SORT BY order_id DESC
-        # LIMIT 10
-        try:
-            offset = 0
-            num    = 10
-            q      = '@client_id:[ {client_id} {client_id} ]'.format(client_id=client_id)
-            query  = (Query( q ).return_fields( 'order_id', 'client_id', 'paid' )
-                      .paging(offset, num).sort_by( 'order_id' , asc=False))
+# Connect to Redis Database
+REDIS_HOST = os.environ[ 'REDIS_HOST' ]
+REDIS_PORT = os.environ[ 'REDIS_PORT' ]
+REDIS_DB   = os.environ[ 'REDIS_DB'   ]
+REDIS_AUTH = os.environ[ 'REDIS_AUTH' ]
 
-            # query = (Query(q).return_fields(  )
-            #          .paging(offset, num).sort_by('order_id', asc=False))
+print( 'basmati.settings.py redis' )
+print( 'REDIS_HOST: {}'.format( REDIS_HOST ) )
+print( 'REDIS_PORT: {}'.format( REDIS_PORT ) )
+print( 'REDIS_DB  : {}'.format( REDIS_DB   ) )
+print( 'REDIS_AUTH: {}'.format( REDIS_AUTH ) )
 
-            orders = self.search( query )
-            return orders
-        except Exception as e:
-            print( 'OrderDaoRedis.select_orders(), error: {}'.format(e) )
+
+conn = redis.Redis(
+     host             = REDIS_HOST
+    ,port             = REDIS_PORT
+    ,db               = REDIS_DB
+    ,password         = REDIS_AUTH
+    ,decode_responses = True
+    )
+
+
+
+db_index = 'product_idx'
+# query = '@password:(123)'
+# results = conn.ft( db_index ).search( query )
+# print( results )
+
+
+
+def select_products( ):
+    # Get the last 10 orders of the client. Only order, no items. See sql
+    #
+    # SELECT *
+    # FROM my_order_index
+    # WHERE client_id =   <client_id>
+    # and row_num between offset and (offset + num)
+    # SORT BY order_id DESC
+    # LIMIT 10
+    try:
+        offset = 0
+        num    = 10
+        where  = '@product_id:[ 1 100 ]'
+        query  = (  Query( where )
+                    .return_fields( 'product_id', 'name', 'pic' )
+                    .paging(offset, num)
+                    .sort_by( 'product_id' , asc=False)
+                  )
+
+        # query = (Query(q).return_fields(  )
+        #          .paging(offset, num).sort_by('order_id', asc=False))
+
+        result = conn.ft( db_index ).search( query )
+
+        print( result )
+
+        return result
+    except Exception as e:
+        print( 'select_products(), error: {}'.format(e) )
+
+# Unit Testing
+
+p = select_products()
+
+print( 'End' )
